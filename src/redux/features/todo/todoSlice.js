ff1ef3c57005840ff347da020623be96c056097f
Enzo <duenzo1010@gmail.com>
2022-01-22 15:07:21 +0800

    feat: add redux/store

---
+++ src/redux/features/todo/todoSlice.js
@@ -0,0 +1,48 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { v4 } from 'uuid'
+
+const initialState = {
+  todos: [
+    { id: v4(), content: "飄浮... 你也會飄浮嗎？", isDone: false },
+    { id: v4(), content: "跳舞小丑潘尼懷斯！", isDone: true }
+  ]
+}
+
+export const todoSlice = createSlice({
+  name: 'todos',
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      state.todos.push({
+        id: v4(),
+        content: action.payload.value,
+        isDone: false
+      })
+    },
+    deleteTodo: (state, action) => {
+      const { todos } = state
+      const index = todos.findIndex((todo) => todo.id === action.payload.id)
+      todos.splice(index, 1)
+    },
+    editTodo: (state, action) => {
+      state.todos.map(todo => {
+        if (todo.id === action.payload.editId) {
+          return todo.content = action.payload.value
+        }
+        return todo
+      })
+    },
+    isDoneToggle: (state, action) => {
+      state.todos.map(todo => {
+        if (todo.id === action.payload.id) {
+          return todo.isDone = !todo.isDone
+        }
+        return todo
+      })
+    }
+  }
+})
+
+export const { addTodo, deleteTodo, editTodo, isDoneToggle } = todoSlice.actions
+export const selectorTodo = state => state.todos.todos
+export default todoSlice.reducer
